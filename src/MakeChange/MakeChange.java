49e8c3eccb52b0b43dbb323809be1c9109029d82
Pierce <pierce.steward@gmail.com>
2020-08-28 17:29:50 -0600

    Main body syntax

--- src/MakeChange/MakeChange.java
+++ src/MakeChange/MakeChange.java
@@ -1,5 +1,41 @@
 package MakeChange;
 
-public class MakeChange {
+import java.util.Scanner;
 
+public class MakeChange {
+	
+	public static void main(String[] args) {
+		
+		Scanner kb=new Scanner(System.in);
+		System.out.println("Please let me know how much the item you are purchasing costs?");
+		System.out.print("$");
+		double item=kb.nextDouble();
+		System.out.println("");
+		System.out.println("**********");
+		System.out.printf("The item you have selected costs $%.2f. %n" , item);
+		System.out.println("**********");
+		System.out.println("");
+		
+		System.out.println("How much money are you paying me with?");
+		System.out.print("$");
+		double pay=kb.nextDouble();
+		System.out.println("");
+		System.out.println("**********");
+		System.out.printf("You have given me $%.2f to pay with. %n" , pay);
+		System.out.println("**********");
+		System.out.println("");
+		
+		if (pay<item) {
+			System.out.println("You have not provided enough to pay for this item.");
+		}
+		else if(pay==item) {
+			System.out.println("Thank you for paying in exact change!");
+		}
+		else {
+			System.out.println("Perfect! Let me get your change!");
+			
+		}
+		
+		kb.close();
+	}
 }
