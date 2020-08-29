39e78d82b118ca5b8325a609b64c000a4dd1f79d
Pierce <pierce.steward@gmail.com>
2020-08-29 11:59:41 -0600

    Final Draft

--- src/MakeChange/MakeChange.java
+++ src/MakeChange/MakeChange.java
@@ -3,89 +3,122 @@ package MakeChange;
 import java.util.Scanner;
 
 public class MakeChange {
-	
+
 	public static void main(String[] args) {
-		
-		Scanner kb=new Scanner(System.in);
+
+		Scanner kb = new Scanner(System.in);
 		System.out.println("Please let me know how much the item you are purchasing costs?");
 		System.out.print("$");
-		double item=kb.nextDouble();
+		double item = kb.nextDouble();
 		System.out.println("");
 		System.out.println("**********");
-		System.out.printf("The item you have selected costs $%.2f. %n" , item);
+		System.out.printf("The item you have selected costs $%.2f. %n", item);
 		System.out.println("**********");
 		System.out.println("");
-		
-		System.out.println("How much money are you paying me with?");
+
+		System.out.println("How much money are you paying with?");
 		System.out.print("$");
-		double pay=kb.nextDouble();
+		double pay = kb.nextDouble();
 		System.out.println("");
 		System.out.println("**********");
-		System.out.printf("You have given me $%.2f to pay with. %n" , pay);
+		System.out.printf("You have given me $%.2f to pay with. %n", pay);
 		System.out.println("**********");
 		System.out.println("");
-		
-		if (pay<item) {
+
+		if (pay < item) {
 			System.out.println("You have not provided enough to pay for this item.");
-		}
-		else if(pay==item) {
+		} else if (pay == item) {
 			System.out.println("Thank you for paying in exact change!");
-		}
-		else {
+		} else {
 			System.out.println("Perfect! Let me get your change!");
+			System.out.println("********************************");
+			double total = ((pay - item) * 100)+.0001;
+			int change = (int) total;
 			
-			double total=(pay-item)*100;
-			int change=(int)total;
-		while (change>0) {
-			if (change% 2000>=0) {
-				int twenty = change/2000;
-				System.out.printf(twenty+" Twenty dollar bills,");
-				change=change-twenty;
+			while (change > 0) {
+				if (change % 2000 >= 0) {
+					int twenty = change / 2000;
+					{
+						if (twenty > 1) {
+							System.out.print("|" + twenty + " Twenty dollar bills| ");
+						} else if (twenty == 1) {
+							System.out.print("|" + twenty + " Twenty dollar bill| ");
+						}
+					}
+					change = change - twenty * 2000;
+				}
+				if (change % 1000 >= 0) {
+					int ten = change / 1000;
+					{
+
+						if (ten > 1) {
+							System.out.print("|" + ten + " Ten dollar bills|");
+						} else if (ten == 1) {
+							System.out.print("|" + ten + " Ten dollar bill|");
+						}
+						change = change - ten * 1000;
+					}
+					if (change % 500 >= 0) {
+						int five = change / 500;
+						if (five > 1) {
+							System.out.print("|" + five + " Five dollar bills|");
+						} else if (five == 1) {
+							System.out.print("|" + five + " Five dollar bill|");
+						}
+						change = change - five * 500;
+					}
+					if (change % 100 >= 0) {
+						int one = change / 100;
+						if (one > 1) {
+							System.out.print("|" + one + " One dollar bills|");
+						} else if (one == 1) {
+							System.out.print("|" + one + " One dollar bill|");
+						}
+						change = change - one * 100;
+					}
+					if (change % 25 >= 0) {
+						int quarter = change / 25;
+						if (quarter > 1) {
+							System.out.print("|" + quarter + " Quarters|");
+						} else if (quarter == 1) {
+							System.out.print("|" + quarter + " Quarter|");
+						}
+						change = change - quarter * 25;
+					}
+					if (change % 10 >= 0) {
+						int dime = change / 10;
+						if (dime > 1) {
+							System.out.print("|" + dime + " Dimes|");
+						} else if (dime == 1) {
+							System.out.print("|" + dime + " Dime|");
+						}
+						change = change - dime * 10;
+					}
+					if (change % 5 >= 0) {
+						int nickle = change / 5;
+						if (nickle > 1) {
+							System.out.print("|" + nickle + " Nickles|");
+						} else if (nickle == 1) {
+							System.out.print("|" + nickle + " Nickle|");
+						}
+						change = change - nickle * 5;
+					}
+					if (change % 1 <= 0) {
+						int penny = change / 1;
+						if (penny > 1) {
+							System.out.print("|" + penny + " Pennies|");
+						} else if (penny == 1) {
+							System.out.print("|" + penny + " Penny|");
+						}
+						change = change - penny * 1;
+
+					}
+				}
+
 			}
-			if(change% 1000<=0) {
-				int ten = change/1000;
-				System.out.printf(ten+" Ten dollar bills,");
-				change=change-ten;
-			}
-			if(change% 500>=0) {
-				int five = change/500;
-				System.out.printf(five+" Five dollar bills,");
-				change=change-five;
-			}
-			if(change% 100>=0) {
-				int one = change/100;
-				System.out.printf(one+" Ten dollar bills,");
-				change=change-one;
-			}
-			if(change% 25>=0) {
-				int quarter = change/25;
-				System.out.printf(quarter+" quarters,");
-				change=change-quarter;
-			}
-			if(change% 10>=0) {
-				int dime = change/10;
-				System.out.printf(dime+" dimes,");
-				change=change-dime;
-			}
-			if(change% 5>=0) {
-				int nickle = change/5;
-				System.out.printf(nickle+" nickles,");
-				change=change-nickle;
-			}
-			if(change% 1>=0) {
-				int penny = change/1;
-				System.out.printf(penny+" pennies.");
-				change=change-penny;
-				break;
-			}
-			 
-			
+
+			kb.close();
 		}
-			
-	
-		
-		kb.close();
+
 	}
-		
-}
 }
